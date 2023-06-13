fc087cfc3b78d75711a685f34b2c178102644d93
Ronald A. Richardson <me@ron.dev>
2023-06-13 22:31:46 +0800

    patch `formatDate` utility test

--- tests/unit/utils/format-date-test.js
+++ tests/unit/utils/format-date-test.js
@@ -4,7 +4,7 @@ import { module, test } from 'qunit';
 module('Unit | Utility | format-date', function () {
     // TODO: Replace this with your real tests.
     test('it works', function (assert) {
-        let result = formatDate();
+        let result = formatDate(new Date());
         assert.ok(result);
     });
 });
