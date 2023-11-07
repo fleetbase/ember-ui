183628e38e4dc430390f95bc29db2b1f89bcea65
TemuulenBM <temuulen.developer@gmail.com>
2023-11-07 17:37:38 +0800

    full-calendar

--- addon/helpers/json-hash.js
+++ addon/helpers/json-hash.js
@@ -1,5 +1,6 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function jsonHash(positional /*, named*/) {
-    return positional;
+export default helper(function jsonHash(positional, named) {
+    let json = JSON.stringify(named);
+    return json;
 });
