1d24e4d49a9c41e7af0a5a5e3a1950d51c5436e5
Ronald A. Richardson <me@ron.dev>
2023-11-24 14:18:11 +0800

    added all arguments available from `<ContentPanel />` to be available to content panels used in the layout sidebars

--- addon/components/content-panel.js
+++ addon/components/content-panel.js
@@ -26,4 +26,18 @@ export default class ContentPanelComponent extends Component {
     @action close() {
         this.isOpen = false;
     }
+
+    @action onDropdownItemClick(action, dd) {
+        if (typeof dd.actions === 'object' && typeof dd.actions.close === 'function') {
+            dd.actions.close();
+        }
+
+        if (typeof action.fn === 'function') {
+            action.fn(action.context);
+        }
+
+        if (typeof action.onClick === 'function') {
+            action.onClick(action.context);
+        }
+    }
 }
