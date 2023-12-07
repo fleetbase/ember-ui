5cd6e81be80e74efb1aca5e3cb01a0201c3ac93c
Ronald A. Richardson <me@ron.dev>
2023-12-07 14:43:40 +0800

    upgraded for ember 5.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,19 @@
 
 module.exports = {
     root: true,
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     parserOptions: {
-        ecmaVersion: 2018,
+        ecmaVersion: 'latest',
         sourceType: 'module',
-        ecmaFeatures: {
-            legacyDecorators: true,
+        requireConfigFile: false,
+        babelOptions: {
+            plugins: [['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }]],
         },
     },
     plugins: ['ember'],
     extends: ['eslint:recommended', 'plugin:ember/recommended', 'plugin:prettier/recommended'],
     env: {
         browser: true,
-        node: true,
     },
     globals: {
         module: 'readonly',
@@ -48,6 +48,7 @@ module.exports = {
             files: [
                 './.eslintrc.js',
                 './.prettierrc.js',
+                './.stylelintrc.js',
                 './.template-lintrc.js',
                 './ember-cli-build.js',
                 './index.js',
@@ -63,13 +64,7 @@ module.exports = {
                 browser: false,
                 node: true,
             },
-            plugins: ['node'],
-            extends: ['plugin:node/recommended'],
-        },
-        {
-            // test files
-            files: ['tests/**/*-test.{js,ts}'],
-            extends: ['plugin:qunit/recommended'],
+            extends: ['plugin:n/recommended'],
         },
     ],
 };
