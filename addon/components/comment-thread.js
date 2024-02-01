4e735b761cb541682eac33d8d963a19fcbbe0f7b
Ronald A. Richardson <me@ron.dev>
2024-02-01 15:48:56 +0800

    added spacer component and patched and completed comment thread component

--- addon/components/comment-thread.js
+++ addon/components/comment-thread.js
@@ -1,27 +1,78 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { isArray } from '@ember/array';
 import { task } from 'ember-concurrency-decorators';
 import getWithDefault from '@fleetbase/ember-core/utils/get-with-default';
 import getModelName from '@fleetbase/ember-core/utils/get-model-name';
 
+/**
+ * Component to handle a thread of comments.
+ */
 export default class CommentThreadComponent extends Component {
+    /**
+     * Service to handle data store operations.
+     * @service
+     */
     @service store;
+
+    /**
+     * Service for handling notifications.
+     * @service
+     */
     @service notifications;
+
+    /**
+     * Service for internationalization.
+     * @service
+     */
+    @service intl;
+
+    /**
+     * The subject related to the comments.
+     * @tracked
+     */
     @tracked subject;
+
+    /**
+     * Array of comments related to the subject.
+     * @tracked
+     */
     @tracked comments = [];
+
+    /**
+     * The text input for publishing a new comment.
+     * @tracked
+     */
     @tracked input = '';
 
+    /**
+     * Context object containing utility functions.
+     */
+    context = {
+        isCommentInvalid: this.isCommentInvalid.bind(this),
+        reloadComments: () => {
+            return this.reloadComments.perform();
+        },
+    };
+
+    /**
+     * Constructor for the comment thread component.
+     * @param owner - The owner of the component.
+     * @param subject - The subject of the comment thread.
+     * @param subjectType - The type of the subject.
+     */
     constructor(owner, { subject, subjectType }) {
         super(...arguments);
 
         this.subject = subject;
-        this.comments = subject.comments;
+        this.comments = getWithDefault(subject, 'comments', []);
         this.subjectType = subjectType ? subjectType : getModelName(subject);
     }
 
+    /**
+     * Asynchronous task to publish a new comment.
+     * @task
+     */
     @task *publishComment() {
         if (this.isCommentInvalid(this.input)) {
             return;
@@ -39,19 +90,28 @@ export default class CommentThreadComponent extends Component {
         this.input = '';
     }
 
+    /**
+     * Asynchronous task to reload the comments related to the subject.
+     * @task
+     */
     @task *reloadComments() {
-        this.comments = yield this.store.query('comment', { subject_uuid: this.subject.id, sort: '-created_at' });
+        this.comments = yield this.store.query('comment', { subject_uuid: this.subject.id, withoutParent: 1, sort: '-created_at' });
     }
 
+    /**
+     * Checks if a comment is invalid.
+     * @param {string} comment - The comment to validate.
+     * @returns {boolean} True if the comment is invalid, false otherwise.
+     */
     isCommentInvalid(comment) {
         if (!comment) {
-            this.notification.warning(this.intl.t('fleet-ops.operations.orders.index.view.comment-input-empty-notification'));
+            this.notifications.warning(this.intl.t('component.comment-thread.comment-input-empty-notification'));
             return true;
         }
 
-        // make sure comment is atleast 12 characters
-        if (typeof comment === 'string' && comment.length < 2) {
-            this.notification.warning(this.intl.t('fleet-ops.operations.orders.index.view.comment-min-length-notification'));
+        // make sure comment is at least 2 characters
+        if (typeof comment === 'string' && comment.length <= 1) {
+            this.notifications.warning(this.intl.t('component.comment-thread.comment-min-length-notification'));
             return true;
         }
 
--- addon/components/comment-thread/comment.js
+++ addon/components/comment-thread/comment.js
@@ -4,44 +4,108 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency-decorators';
 
+/**
+ * Component to handle individual comments in a comment thread.
+ */
 export default class CommentThreadCommentComponent extends Component {
+    /**
+     * Service to handle data store operations.
+     * @service
+     */
     @service store;
+
+    /**
+     * The text input for replying or editing comments.
+     * @tracked
+     */
     @tracked input = '';
+
+    /**
+     * Flag to indicate if the reply interface is active.
+     * @tracked
+     */
     @tracked replying = false;
+
+    /**
+     * Flag to indicate if the edit interface is active.
+     * @tracked
+     */
     @tracked editing = false;
 
-    constructor(owner, { comment }) {
+    /**
+     * The constructor for the comment component.
+     * @param owner - The owner of the component.
+     * @param comment - The comment data for the component.
+     */
+    constructor(owner, { comment, contextApi }) {
         super(...arguments);
-        
+
         this.comment = comment;
+        this.contextApi = contextApi;
     }
 
+    /**
+     * Activates the reply interface.
+     * @action
+     */
     @action reply() {
         this.replying = true;
     }
 
+    /**
+     * Deactivates the reply interface.
+     * @action
+     */
     @action cancelReply() {
         this.replying = false;
     }
 
+    /**
+     * Activates the edit interface.
+     * @action
+     */
     @action edit() {
         this.editing = true;
     }
 
+    /**
+     * Deactivates the edit interface.
+     * @action
+     */
     @action cancelEdit() {
         this.editing = false;
     }
 
+    /**
+     * Deletes the current comment.
+     * @action
+     */
     @action delete() {
         this.comment.destroyRecord();
     }
 
+    /**
+     * Asynchronous task to update the current comment.
+     * @task
+     */
     @task *updateComment() {
+        if (this.contextApi && this.contextApi.isCommentInvalid(this.comment.content)) {
+            return;
+        }
+
         yield this.comment.save();
         this.editing = false;
     }
 
+    /**
+     * Asynchronous task to publish a reply to the current comment.
+     * @task
+     */
     @task *publishReply() {
+        if (this.contextApi && this.contextApi.isCommentInvalid(this.input)) {
+            return;
+        }
+
         let comment = this.store.createRecord('comment', {
             content: this.input,
             parent_comment_uuid: this.comment.id,
@@ -56,6 +120,10 @@ export default class CommentThreadCommentComponent extends Component {
         this.input = '';
     }
 
+    /**
+     * Asynchronous task to reload replies to the current comment.
+     * @task
+     */
     @task *reloadReplies() {
         this.comment = yield this.comment.reload();
     }
