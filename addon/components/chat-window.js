340ae0ecfd0c5afd2257a038bb30a08a973e6531
Doljinsuren Enkhbayar <doljko927@gmail.com>
2024-04-03 13:28:08 +0800

    removed function

--- addon/components/chat-window.js
+++ addon/components/chat-window.js
@@ -1,20 +1,3 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 
-export default class ChatWindowComponent extends Component {
-    @service socket;
-    @service chat;
-    @tracked channel;
-    constructor(owner, { chatChannel }) {
-      super(...arguments);
-      this.channel = chatChannel;
-      this.listen();
-    }
-  
-    listen() {
-      this.socket.listen(`chat.${this.channel.public_id}`, (event) => {
-        console.log('[chat event]', event);
-      });
-    }
-  }
+export default class ChatWindowComponent extends Component {}
