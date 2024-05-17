d5946d39d41555434a3b2932b231bf6198fe05da
Ronald A. Richardson <me@ron.dev>
2024-05-17 12:59:56 +0800

    fix `<FileIcon />` component to handle `UploadedFile` instances too

---
+++ tests/unit/utils/is-upload-file-test.js
@@ -0,0 +1,10 @@
+import isUploadFile from 'dummy/utils/is-upload-file';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | is-upload-file', function () {
+    // TODO: Replace this with your real tests.
+    test('it works', function (assert) {
+        let result = isUploadFile();
+        assert.ok(result);
+    });
+});
