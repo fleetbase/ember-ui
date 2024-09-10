7aced9b848f6b430d79ef0e56213e8ac557dad85
Ronald A. Richardson <me@ron.dev>
2024-09-10 20:00:02 +0800

    fix tests

--- tests/unit/services/dashboard-test.js
+++ tests/unit/services/dashboard-test.js
@@ -6,6 +6,12 @@ module('Unit | Service | dashboard', function (hooks) {
 
     // TODO: Replace this with your real tests.
     test('it exists', function (assert) {
+        // register dummy services
+        this.owner.register('service:store', {}, { instantiate: false });
+        this.owner.register('service:fetch', {}, { instantiate: false });
+        this.owner.register('service:notifications', {}, { instantiate: false });
+        this.owner.register('service:intl', {}, { instantiate: false });
+        this.owner.register('service:universe', {}, { instantiate: false });
         let service = this.owner.lookup('service:dashboard');
         assert.ok(service);
     });
