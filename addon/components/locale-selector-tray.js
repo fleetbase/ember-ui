3e3c7d7f73e83dfe6700df53d4ba5fcf8f3dd29d
Ronald A. Richardson <me@ron.dev>
2025-12-19 16:21:26 +0800

    fix linter

--- addon/components/locale-selector-tray.js
+++ addon/components/locale-selector-tray.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
-import { debug } from '@ember/debug';
 import { task } from 'ember-concurrency';
 import calculatePosition from 'ember-basic-dropdown/utils/calculate-position';
 
