7f5af2e984f267a3069903db27749ba8237d4474
Ronald A. Richardson <me@ron.dev>
2026-09-15 22:05:16 +0800

    Add the user resource family
    user/pill, user/summary, select-option/user and table/cell/user-identity, as wrappers over the resource identity bases for the user descriptor.

--- addon/components/user/pill.js
+++ addon/components/user/pill.js
@@ -1,7 +1,40 @@
 import Component from '@glimmer/component';
+import { get } from '@ember/object';
 
+/**
+ * The user family pill. Keeps the historic `@user` argument and "No user" /
+ * email defaults, and is otherwise a `Resource::Pill` for the `user`
+ * descriptor: it opens the user when an engine has registered an opener and
+ * shows the user's summary on hover.
+ */
 export default class UserPillComponent extends Component {
     get resource() {
         return this.args.user ?? this.args.resource;
+    }
+
+    get subtitle() {
+        if (this.args.subtitle !== undefined) {
+            return this.args.subtitle;
+        }
+
+        const email = get(this.resource ?? {}, 'email');
+
+        return email ?? (this.resource ? 'No email' : '-');
+    }
+
+    get showOnlineIndicator() {
+        if (this.args.showOnlineIndicator !== undefined) {
+            return Boolean(this.args.showOnlineIndicator);
+        }
+
+        return typeof this.args.onlinePath === 'string';
+    }
+
+    get online() {
+        if (typeof this.args.onlinePath === 'string' && this.resource) {
+            return Boolean(get(this.resource, this.args.onlinePath));
+        }
+
+        return undefined;
     }
 }
